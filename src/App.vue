c136f6c9ff48bdde2829547aad2e64647dade7b9
rohan-compro <108351915+rohan-compro@users.noreply.github.com>
2023-03-24 11:28:27 +0530

    Revert "home page updated"

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
   </header>
 
   <main>
-    <h1>
-      Hello Everyone!!
-    </h1>
+    <TheWelcome />
   </main>
 </template>
 
@@ -35,5 +38,10 @@ header {
     margin: 0 2rem 0 0;
   }
 
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
